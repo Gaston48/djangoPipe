dbd3d8671ce1a0cd89983b19e24c16ef49f25120
Ingo <ingo@von-komorowski.name>
2021-12-02 18:42:03 +0100

    tests added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    def get_author(self):
+        return self.author
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from . models import Post
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create(title='django', author='djangoAuthor', slug='djangoSlug')
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+
+    def test_post_return_title_on_default(self):
+        d = self.blog
+        self.assertEqual(str(d), 'django')
+
+    def test_get_author(self):
+        d = self.blog
+        self.assertEqual(d.get_author(), 'djangoAuthor')
